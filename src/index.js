e01eb32dc057f9cac6c14a8ead50561b5714dc64
Ina Davidova <ina.davidovaa@gmail.com>
2022-03-31 03:43:22 +0300

    Setup server and add the .hbs pages

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log('jds');
+const express = require('express');
+const { create: handlebars } = require('express-handlebars');
+const path = require('path');
+
+const app = express();
+
+app.set('views', path.resolve('./src/views'));
+
+app.engine('hbs', handlebars({
+    extname: 'hbs'
+}).engine);
+
+app.set('view engine', 'hbs');
+
+app.all('/', (req, res)=>{
+    res.render('index');
+});
+
+app.listen(5000, ()=> console.log('Server is running on port 5000'))
